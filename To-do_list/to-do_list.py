ca77370e05d12f0cbe7aaf4f3217b4a206416e64
assil-coder <ziarmohammedassil@gmail.com>
2026-02-18 17:55:50 +0100

    Project 4

---
+++ To-do_list/to-do_list.py
@@ -0,0 +1,49 @@
+tasks=[]
+
+while True:
+
+
+    print("""=== TO-DO LIST ===
+    1. Add Task
+    2. View Tasks
+    3. Mark Task as Complete
+    4. Delete Task
+    5. Exit""")
+
+    user_option=input("\nchoose an option (1)(2)(3)(4)(5): ")
+            
+    if user_option=="1":
+        number_tasks=int(input("How many tasks do you want to add? "))
+        for i in range(1,number_tasks+1):
+            adding_task=input("Enter the name of the task that you want to add to your to do list:")    
+            tasks.append(adding_task)
+            
+    elif user_option=="2":
+            print(tasks)
+
+    elif user_option=="3":
+            print(tasks)
+            done_tasks=input("Enter the name of the task that you've done:")
+            if done_tasks in tasks:
+                  
+                tasks.remove(done_tasks)
+                print(f"Here are your remaining tasks {tasks}")
+            else:
+                  print("task not found!")
+    elif user_option=="4":
+            print(tasks)
+            deleting_tasks=input("Enter the name of the tasks that you want to delete:")
+            if deleting_tasks in tasks : 
+                tasks.remove(deleting_tasks)
+                print(f"Here are your remainig tasks {tasks}")
+            else :
+                  print("task not found!")
+        
+    elif user_option=="5":
+            break
+
+    else:
+        print("Error!")
+
+
+
