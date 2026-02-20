7944400fabc43ec6204c965f0f86a3e691da142c
assil-coder <ziarmohammedassil@gmail.com>
2026-02-20 16:38:54 +0100

    Project 5 ( unfinished )

---
+++ expenses_tracker/expenses_tracker.py
@@ -0,0 +1,11 @@
+
+print("""=== EXPENSE TRACKER ===
+1. Add Expense
+2. View All Expenses
+3. View Total Spending
+4. View Spending by Category
+5. Exit""")
+
+user_option=input("\nChoose an option(1)(2)(3)(4)(5): ")
+if user_option == "1":
+    
