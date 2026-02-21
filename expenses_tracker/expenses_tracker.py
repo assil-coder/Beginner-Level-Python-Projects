6717ae42847ec269952a1759e285185c932b5334
assil-coder <ziarmohammedassil@gmail.com>
2026-02-21 15:57:23 +0100

    Project 5

--- expenses_tracker/expenses_tracker.py
+++ expenses_tracker/expenses_tracker.py
@@ -1,11 +1,43 @@
-
-print("""=== EXPENSE TRACKER ===
+menu= """========== EXPENSE TRACKER ==========
 1. Add Expense
 2. View All Expenses
 3. View Total Spending
-4. View Spending by Category
-5. Exit""")
+4. Exit\n\n"""
 
-user_option=input("\nChoose an option(1)(2)(3)(4)(5): ")
-if user_option == "1":
+expenses=[]
+
+while True:
+    print(menu)
     
+    choice=input("Choose an option:")
+    
+    if choice == "1":
+        amount=int(input("Enter amount: "))
+        category=input("Enter category food/entertainement/transport/other : ")
+
+        expenses.append({"amount":amount ,"category":category})
+        print("Expense added!")
+    
+    elif choice == "2":
+        if len(expenses) == 0:
+            print("No expenses yet!")
+        else:
+            for expense in expenses:
+                print(f"{expense['category']} - ${expense['amount']}")
+
+
+
+
+    elif choice == "3":
+        total = 0
+        for expense in expenses:
+            total += expense['amount']
+        print(f"Total spending: ${total}")
+       
+
+    elif choice == "4":
+        print("goodbye!")
+        break
+    
+    else:
+        print("Error!")
